4b6f7f33ba374bbb18cdba713e0e31b08f5081ed
Eric Kennedy <ekeric13@gmail.com>
2014-12-10 15:26:30 -0800

    completed functional stack push and pop

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,12 +3,18 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    counter += 1;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    counter -= 1;
+    var popVal = storage[counter];
+
   };
 
   someInstance.size = function(){
