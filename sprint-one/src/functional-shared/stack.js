0328dc99924e7d46e5068e73de18ef7201283006
Eric Kennedy <ekeric13@gmail.com>
2014-12-10 19:54:14 -0800

    completed stack functional shared push, pop, size

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Define someInstance, storage, count
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  // Extend stackMethods
+  _.extend(someInstance, stackMethods);
+  // Return someInstance
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Here will go push/pop/size
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count += 1
+  },
+
+  pop: function() {
+    if (this.count){
+      this.count -= 1;
+    }
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+
+  },
+  size: function() {
+    return this.count;
+  }
+};
+
 
 
