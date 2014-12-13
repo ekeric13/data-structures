678c772e25f7d6450ed4b4e93d3d18227411f932
Eric Kennedy <ekeric13@gmail.com>
2014-12-12 19:41:48 -0800

    troubleshooting floobits problem

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,33 +2,34 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(queueMethods);
-  someInstance.storage = {};
-  someInstance.count = 0;
-  someInstance.key = 0;
+
+
+  someInstance._storage = {};
+
+  someInstance._start = 0;
+
+  someInstance._end = 0;
+
   return someInstance;
 
 };
 
-var queueMethods = {
-  enqueue : function(value){
-    this.storage[this.key] = value;
-    this.count += 1;
-    this.key += 1;
-  },
+var queueMethods = {};
 
-  dequeue : function(){
-    if (this.count){
-      this.count -= 1;
-    }
-    var lowestKey = Object.keys(this.storage).sort()[0];
-    var result = this.storage[lowestKey];
-    delete this.storage[lowestKey];
-    return result;
-  },
 
-  size : function(){
-    return this.count;
-  }
+queueMethods.enqueue = function(value){
+    this.storage[this.end++] = value;
 };
 
+queueMethods.dequeue = function(){
+  var result = this.storage[this.start];
+  if (this.size() ){
+    this.start++
+    delete this.storage[this.start]
+  }
+  return result;
+};
 
+queueMethods.size = function(){
+  return this.end - this.start;
+};
